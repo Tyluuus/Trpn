5448e86ba055eb461bb4eab08ae461a7e08efe74
Tyluuus <tylus97@gmail.com>
2022-12-19 22:45:52 +0100

    Initial Commit

---
+++ Trppn/TrppnApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrppnApp.swift
+//  Trppn
+//
+//  Created by Piotr Tyl on 19/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct TrppnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
